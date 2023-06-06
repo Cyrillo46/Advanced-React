46f0285f58a5460fc78535c61840746952f3e29e
Cyrillo46 <yahooisbetter46@gmail.com>
2023-06-06 16:34:59 -0400

    Set count variable and count incrementor.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import Starter from "./tutorial/01-useState/starter/01-error-example";
 
+let count = 0;
+
+const handleClick = () => {
+  count += 1;
+  console.log("Click!");
+};
+
 function App() {
   return (
     <div className="container">
       <Starter />
+      <button
+        increment={handleClick}
+        style={{
+          padding: "1em",
+          width: "5em",
+          backgroundColor: "green",
+          border: "none",
+          borderRadius: "5px",
+        }}>
+        {count}
+      </button>
     </div>
   );
 }
