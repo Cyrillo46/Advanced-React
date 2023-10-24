3927889b42797d8e0e24257f72b3336beb5d839a
Cyrillo46 <yahooisbetter46@gmail.com>
2023-10-24 15:05:53 -0400

    successfully fetched user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import CodeExample from "./tutorial/02-useEffect/starter/01-code-example.jsx";
-import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics.jsx";
-
+import FetchData from "./tutorial/02-useEffect/starter/04-fetch-data.jsx";
+import { useState, useEffect } from "react";
 function App() {
   return (
     <div className="container">
-      <CodeExample />
+      <FetchData />
     </div>
   );
 }
