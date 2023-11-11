634867e9d56ba8c63320b18f29f346afdc4e66d1
Cyrillo46 <yahooisbetter46@gmail.com>
2023-11-11 13:27:01 -0500

    corrected login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx";
+import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge.jsx";
 function App() {
   return (
     <div className="container">
